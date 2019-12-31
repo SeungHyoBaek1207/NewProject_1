61e974f44454d20b126d725d80ffab184d7efd26
승효 백 <likesonnet@msn.com>
2019-12-31 13:07:33 +0900

    1st commit.

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Public Sub BSH1()
+        Dim V1 As Single
+        Dim V2 As Single
+        Dim V3 As Single
+
+        V1 = 10
+        V2 = 30
+        V3 = V1 + V2
+
+    End Sub
+
+End Class
